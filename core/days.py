27fc965c2e0387a0c1545e8af2ebaf0c2fb63063
analogash <i.megaminx@gmail.com>
2025-04-17 23:35:35 +0100

    Update: day_zero

--- core/days.py
+++ core/days.py
@@ -19,17 +19,28 @@ def day_zero(character):
         say(dialog["day_zero"]["1st_choice"][1])
         choice_1 = multiple_choice(A="look", B="go")
 
-        if choice_1 == "look":
+        if choice_1 is "look":
             narrate(look_list[counter], 3)
             counter = (counter + 1) % len(look_list)
 
-        if choice_1 == "go":
-            narrate(actions["intro_go"][character["background"]])
+        if choice_1 is "go":
+            for i in range(0, len(actions["intro_go"][character["background"]])):
+                say(actions["intro_go"][character["background"]][i], 4)
             break
 
+    while True:
+        say(actions["herb_look_options"][0])
+        say(actions["herb_look_options"][1])
 
+        choice_2 = multiple_choice(A="look", B="collect", C="go")
 
+        if choice_2 is "look":
+            narrate(actions["herbs_look"][character["background"]], 3)
 
+        if choice_2 is "collect":
+            say(actions["herbs_collect"][character["background"]], 4)
 
+        if choice_2 is "go":
+            break
 
     #print(f"DEBUG: {choice_1}")
